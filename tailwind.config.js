b4255dc93797a4f09a420816f0901840ed8ccec1
Ashkan <141755372+ashkannaseri2112@users.noreply.github.com>
2024-05-21 12:46:14 +0330

    fix homePage Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,34 +3,33 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
-      colors:{
-        green:{
-          350:"#84CC16"
-        }
+      colors: {
+        green: {
+          350: "#84CC16",
+        },
+      },
+      backgroundImage: {
+        "homePage-Img": "url(/public/images/Home.png)",
       },
     },
     screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
     fontFamily: {
       Poppins: "Poppins",
       PoppinsRegular: "Poppins Regular",
-      PoppinsSemibold:"Poppins Semibold",
-      PoppinsBold:"Poppins Bold",
-      PoppinsBlack:"Poppins Black",
+      PoppinsSemibold: "Poppins Semibold",
+      PoppinsBold: "Poppins Bold",
+      PoppinsBlack: "Poppins Black",
     },
     container: {
       center: true,
       padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
+        DEFAULT: "1rem",
       },
     },
   },
